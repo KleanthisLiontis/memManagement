e8a48510dacdd3a88ad42244620eada71eb2ad8e
KleanthisLiontis <kleanthisliontis@gmail.com>
2023-10-01 20:18:17 +0300

    Typecast and calculations

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let a = 2;
+    let a = 75;
     let result = stack_only(a);
     dbg!(result);
+ 
+    println!("Small function test OG weight:{} and adjusted Mars weight:{}", a, you_on_mars(a));
 }
 
 fn stack_only(b: i32) -> i32 {
@@ -13,4 +15,8 @@ fn stack_and_heap() -> i32 {
     let d = 5;
     let e = Box::new(7);
     return d + *e;
+}
+
+fn you_on_mars(_earth_weight: i32) -> f32 {
+    return _earth_weight as f32/9.81 *3.711;
 }
